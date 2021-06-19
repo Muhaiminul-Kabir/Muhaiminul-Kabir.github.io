56ddc537e1b92f59f5863d3ff0692662a2d9c8c2
Muhaiminul-Kabir <muhaiminulkabir32@gmail.com>
2021-06-19 13:34:37 +0600

    vjvjv

---
+++ hello.js
@@ -0,0 +1,14 @@
+var x = 2+7+7+7+7+7+8+8+8+8+8+9;
+var y = 'gsyshshus';
+
+var a;
+var c = prompt('enter your name',"name");
+alert("welcom "+ c);
+name();
+
+function name(){
+  for (var i = 0; i < 12; i++) {
+    document.write("<h1><script> document.write(c)</script</h1>");
+    document.write('<br>');
+  }
+}
